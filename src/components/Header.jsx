41cd4a887c90fa260d4023a4ff947feaf13838b9
Mike Haid <haidmichaelj@gmail.com>
2025-09-22 15:05:10 -0500

    Added Shadcn to use javascript, added a button to test connection.

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+const Header = () => {
+    return (
+        <div className="header">
+            {/* <Link> */}
+                <span>Sign In/Register</span>
+                <span>Log Out</span>
+            {/* </Link>  */}
+        </div>
+)
+}
+
+export default Header 
